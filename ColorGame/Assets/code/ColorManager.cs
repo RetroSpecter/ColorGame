4b340596f9e4f12fdfc7f67f662131676ecef1da
Samuel Cochrane <SamuelDavisCochrane@gmail.co>
2015-10-30 02:17:58 -0700

    adding some UI and GameManagement

--- ColorGame/Assets/code/ColorManager.cs
+++ ColorGame/Assets/code/ColorManager.cs
@@ -1,17 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class ColorManager : MonoBehaviour {
 
-	enum Color {White, Red, Green, Blue};
-	public Color curColor;
+	public enum GameColor {White, Red, Green, Blue};
+    public GameColor curColor;
+	public GameColor curSelectedColor;
+
+    [SerializeField] private bool rotatingColors;
+    public float rotateEveryXSeconds;
+    private float secondsUntilRotation;
+    
+    public bool[] onColors = { true, false, false, false };
 	// Use this for initialization
 	void Start () {
-	
+        rotateColor();
+        secondsUntilRotation = rotateEveryXSeconds;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (rotatingColors) { 
+            if (secondsUntilRotation <= 0) {
+                secondsUntilRotation = rotateEveryXSeconds;
+                rotateColor();
+            }
+            secondsUntilRotation -= Time.deltaTime;
+        }
 	}
+
+    public void toggleRotation() {
+        rotatingColors = !rotatingColors;
+        secondsUntilRotation = rotateEveryXSeconds;
+    }
+
+
+
+
+    //rotates or current color to the next ON color
+    public void rotateColor() {
+        //if we haven't unlocked any colors, dont do anything
+        if (!(onColors.Count(c => c) <= 1)) {
+            int currentColor = (int)curSelectedColor;
+            int nextColor= getNextColorInt(currentColor);
+            //while (!onColors[nextColor]) {
+            //    nextColor = getNextColorInt();
+            //}
+        
+            while (!onColors[nextColor]) {
+                nextColor = getNextColorInt(nextColor);
+            }
+            
+            curSelectedColor = (GameColor)nextColor;
+
+        }
+
+    }
+
+    //returns the int value of the color after our current color
+    //helper method for rotatecolor();
+    private int getNextColorInt(int currentColor) {
+        int nextColor;
+        
+        if (currentColor == 3) { 
+            nextColor = 0; 
+        } else { 
+            nextColor = currentColor + 1; 
+        }
+
+        return nextColor;
+
+    }
+
+    public float getSecondsUntilRotation() {
+        return secondsUntilRotation;
+    }
+
 }
