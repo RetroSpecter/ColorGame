da113e463e60c709eaa75497c6c4c7c8ef9149c8
RetroSpecter <InsertNameHere4897@gmail.com>
2015-10-30 15:05:08 -0700

    PlayerController
    Created simple character and character movment

---
+++ ColorGame/Assets/code/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float runSpeed;
+	public float jumpHeight;
+	public float maxJumpHeight;
+	private Rigidbody2D myRigidBody2D;
+	private float moveVelocity;
+	
+	public Transform groundCheck;
+	public float groundcheckRadius;
+	public LayerMask WhatisGround;
+	private bool grounded;
+
+
+	void Start () {
+		myRigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundcheckRadius, WhatisGround);
+	}
+	void Update () {
+
+		//Left and Right Movement
+		moveVelocity = 0f;
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+				moveVelocity = runSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+				moveVelocity = -runSpeed;
+			} 
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
+
+
+
+			//Jumping
+			if (Input.GetKeyDown (KeyCode.UpArrow) && grounded) {
+				Jump ();
+			}
+	
+		}
+	public void Jump(){
+		myRigidBody2D.velocity = new Vector2 (myRigidBody2D.velocity.y,jumpHeight);
+	}
+}
