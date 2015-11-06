bba8c0ef4244e63c17d24d5dc6b5acfc588f136e
Samuel Cochrane <SamuelDavisCochrane@gmail.co>
2015-11-05 17:22:01 -0800

    spiny orb ayyy

---
+++ ColorGame/Assets/PixelPerfect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class PixelPerfect : MonoBehaviour {
+
+    public float textureSize = 100f;
+
+    float unitsPerPixel;
+
+    // Use this for initialization
+
+    void Start() {
+
+        unitsPerPixel = 1f / textureSize;
+
+        Camera.main.orthographicSize = (Screen.height / 2f) * unitsPerPixel;
+
+    }
+
+}
