7d8c20fb6f25ca7b1c4dd35e00478477d58636ff
RetroSpecter <InsertNameHere4897@gmail.com>
2015-11-16 20:02:00 -0800

    playing around with tiling/background objects

---
+++ ColorGame/Assets/code/BackgroundScroll.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z);
+	}
+}
