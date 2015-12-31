6435580477908770b1512395dbe4c1de698ac089
RetroSpecter <InsertNameHere4897@gmail.com>
2015-12-31 14:53:51 -0800

    Revert "Revert "+ screenFlash on color change, camera tweaks, background now autoscales to fit screen""
    This reverts commit a16e732f75c5aabee38c917824419a61adb4c28d.

---
+++ ColorGame/Assets/ColorChangeScreenFlash.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChangeScreenFlash : MonoBehaviour
+{
+
+
+
+    public Color[] colors = new Color[5];
+
+
+    private Renderer rend;
+    private Collider2D coll;
+    private ColorManager CM;
+    private bool isOn;
+    private int color;
+
+    // Awake
+    void Awake()
+    {
+        CM = GameObject.Find("GameManager").GetComponent<ColorManager>();
+        rend = this.GetComponent<Renderer>();
+        // Start the event listener
+        //if (usingSecondaryColor) {
+        //CM.OnSecondaryColorChange += OnColorChange;
+        //}
+        //else { 
+        CM.OnColorChange += OnColorChange;
+        //}
+        OnColorChange((int)CM.curUsingColor);
+    }
+    //public bool[] activeColors = { true, false, false, false };
+
+    public void OnColorChange(int col)
+    {
+        color = col;
+        //if we changed to a color we are on for, and are not currently on
+        changeTexture(col);
+        spawnParticle();
+    }
+
+    public void finishLevel()
+    {
+        OnColorChange(5);
+    }
+
+
+    public void changeTexture(int col)
+    {
+        print("changeTint");
+        rend.material.SetColor("_TintColor", colors[col]);
+
+        //iTween.ValueTo(gameObject, , );
+
+        //iTween.ColorTo(gameObject, colors[col], 0.25f);
+        StartCoroutine("FadeOut");
+    }
+
+    IEnumerator FadeOut()
+    {
+        print("fading");
+        float time = 0.5f; //time it should take for the entire 'flash'
+        float a = 0;
+        float maxA = 0.09f; //most opaque this flash will get (1 - fully opaque, 0 - fully transulcent)
+
+        Color c = rend.material.GetColor("_TintColor");
+        c.a = a;
+
+        //bring the opacity up to the maximum value
+        while (a < maxA)
+        {
+            a += Time.deltaTime / (time/2); //print(a + "   a");
+            c.a = a;
+
+            rend.material.SetColor("_TintColor", c);
+            yield return null;
+        }
+
+        //then back down again to 0 (transparent
+        while (a > 0)
+        {
+            a -= Time.deltaTime / (time/2); //print(a + "   a");
+            c.a = a;
+          
+            rend.material.SetColor("_TintColor", c);
+            yield return null;
+        }
+        
+
+    }
+
+public void spawnParticle()
+    {
+    //    if (blockParticle != null && rend != null)
+    //    {
+    //        Instantiate(blockParticle, transform.position, Quaternion.Euler(270, 0, 0));
+    //    }
+    }
+
+}
--- ColorGame/Assets/code/BackgroundScroll.cs
+++ ColorGame/Assets/code/BackgroundScroll.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class BackgroundScroll : MonoBehaviour {
 	public GameObject player;
+    public Camera cam;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+
+        if (cam == null)
+        {
+            this.enabled = false;
+        }
+        
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z);
-	}
+        transform.localScale = new Vector3(cam.orthographicSize * (Screen.width / Screen.height) * 0.45f, cam.orthographicSize * (Screen.width / Screen.height) * 0.45f, 1);
+
+    }
 }
